fc4afe677a7e823c1eaabc4edd06fa837e5b80a4
alkihuri <alkihuri@gmail.com>
2023-11-12 13:42:51 +0100

    Update Bot_api.py

--- BotEngine/Bot_api.py
+++ BotEngine/Bot_api.py
@@ -14,8 +14,7 @@ async def send_to_chat(message: types.Message):
 
 @dp.message_handler(content_types=types.ContentTypes.TEXT)
 async def text_messages(message: types.Message):
-
-    await bot.send_message(chat_id=message.chat.id, text='Отправьте картинку ошибки сюда (желательно с описанием)')
-
+    await bot.send_message(chat_id=Config.programmers_chat_id, text=message.text)
+    await bot.send_message(chat_id=message.chat.id, text='Лучше отправить картинку ошибки сюда (желательно с описанием), но ошибка отправлена и скоро будет исправлена')
 if __name__ == '__main__':
     executor.start_polling(dispatcher=dp, skip_updates=True)
