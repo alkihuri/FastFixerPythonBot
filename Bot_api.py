10f2eddf1d1ab7e1c021d33a1d452d1049d9da43
alkihuri <alkihuri@gmail.com>
2023-11-13 23:20:18 +0100

    Update Bot_api.py

--- Bot_api.py
+++ Bot_api.py
@@ -9,7 +9,8 @@ dp = Dispatcher(bot)
 @dp.message_handler(content_types=types.ContentTypes.PHOTO)
 async def send_to_chat(message: types.Message):
 
-    await bot.send_photo(chat_id=Config.programmers_chat_id, photo=message.photo[-1].file_id, caption=message.caption)
+    msg = message.from_user + ":"  + message.caption
+    await bot.send_photo(chat_id=Config.programmers_chat_id, photo=message.photo[-1].file_id, caption=msg)
     await bot.send_message(chat_id=message.chat.id, text='Ошибка отправлена и скоро будет исправлена')
 
 @dp.message_handler(content_types=types.ContentTypes.TEXT)
